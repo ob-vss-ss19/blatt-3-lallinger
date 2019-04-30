9a5e3fe48daeeae3f5fe2c580c3c8edec71bbc00
Lallinger <lallinger.patrick@web.de>
2019-04-30 11:20:49 +0200

    created first messages

--- treecli/main.go
+++ treecli/main.go
@@ -3,18 +3,73 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sync"
+	"time"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	"github.com/ob-vss-ss19/blatt-3-lallinger/tree/"
+	"github.com/ob-vss-ss19/ob-vss-ss19/proto.actor/echomessages"
 )
 
-var id *int
-var token *string
-var forceDelete *bool
+type MyActor struct {
+	count int
+	wg    *sync.WaitGroup
+}
+
+func (state *MyActor) Receive(context actor.Context) {
+	switch context.Message().(type) {
+	case *echomessages.Response:
+		state.count++
+		fmt.Println(state.count)
+	case *actor.Stopped:
+		state.wg.Done()
+	}
+}
+
+var (
+	// nolint:gochecknoglobals
+	flagBind = flag.String("bind", "localhost:8090", "Bind to address")
+	// nolint:gochecknoglobals
+	flagRemote = flag.String("remote", "localhost:8091", "remote host:port")
+)
+
+var id = flag.Int("id", -1, "tree id")
+var token = flag.String("token", "", "tree token")
+var forceDelete = flag.Bool("no-preserve-tree", false, "force deletion of tree")
 
 func main() {
 
-	id = flag.Int("id", -1, "tree id")
-	token = flag.String("token", "", "tree token")
-	forceDelete = flag.Bool("no-preserve-tree", false, "force deletion of tree")
 	flag.Parse()
+
+	remote.Start(*flagBind)
+	var wg sync.WaitGroup
+	props := actor.PropsFromProducer(func() actor.Actor {
+		wg.Add(1)
+		return &MyActor{0, &wg}
+	})
+	rootContext := actor.EmptyRootContext
+	pid := rootContext.Spawn(props)
+	message := &echomessages.Echo{Message: "hej"}
+
+	fmt.Println("Sleeping 5 seconds...")
+	time.Sleep(5 * time.Second)
+	fmt.Println("Awake...")
+
+	//this is the remote actor we want to communicate with
+	fmt.Printf("Trying to connect to %s\n", *flagRemote)
+
+	pidResp, err := remote.SpawnNamed(*flagRemote, "remote", "hello", 5*time.Second)
+	if err != nil {
+		panic(err)
+	}
+	remotePid := pidResp.Pid
+
+	for i := 0; i < 10; i++ {
+		rootContext.RequestWithCustomSender(remotePid, message, pid)
+	}
+
+	wg.Wait()
 
 	switch flag.Args()[0] {
 	case "newtree":
--- treeservice/main.go
+++ treeservice/main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
-	console "github.com/AsynkronIT/goconsole"
+	"flag"
+	"sync"
+
 	"github.com/AsynkronIT/protoactor-go/actor"
-	"github.com/ob-vss-ss19/blatt-3-lallinger/tree"
+	"github.com/AsynkronIT/protoactor-go/log"
+	"github.com/AsynkronIT/protoactor-go/remote"
+	"github.com/ob-vss-ss19/ob-vss-ss19/proto.actor/echomessages"
 )
 
+type MyActor struct{}
+
+func (state *MyActor) Receive(context actor.Context) {
+	switch context.Message().(type) {
+	case *echomessages.Echo:
+		context.Respond(&echomessages.Response{
+			SomeValue: "result",
+		})
+	default: // just for linter
+	}
+}
+
+func NewMyActor() actor.Actor {
+	log.Message("Hello-Actor is up and running")
+	return &MyActor{}
+}
+
+// nolint:gochecknoglobals
+var flagBind = flag.String("bind", "localhost:8091", "Bind to address")
+
 func main() {
-	context := actor.EmptyRootContext
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+
+	flag.Parse()
+	remote.Start(*flagBind)
+
+	remote.Register("hello", actor.PropsFromProducer(NewMyActor))
+
+	/*context := actor.EmptyRootContext
 	props := actor.PropsFromProducer(func() actor.Actor {
 		return &tree.NodeActor{Id: 1, LeafSize: 2} // nolint:errcheck
 	})
 	pid := context.Spawn(props)
 	context.Send(pid, &tree.Add{Value: "hallo", Key: 4})
 	context.Send(pid, &tree.Find{Key: 4})
-	console.ReadLine() // nolint:errcheck
+	console.ReadLine() // nolint:errcheck*/
 }
